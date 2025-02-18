6ec20267a4e622945b9e904fccaeee8eef4b6583
Artie <artierosser@gmail.com>
2025-02-18 22:41:19 +0000

    test larger addition

--- src/test/initial.test.tsx
+++ src/test/initial.test.tsx
@@ -1,26 +1,43 @@
 /** @format */
 
 import { render, screen } from "@testing-library/preact";
-import userEvent from "@testing-library/user-event";
+import userEvent, { UserEvent } from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
 import { App } from "../app";
+
+function getButton(name: string) {
+  return screen.findByRole("button", { name });
+}
+
+function makeClickableButton(user: UserEvent) {
+  return async function clickButton(name: string) {
+    return user.click(await getButton(name));
+  };
+}
 
 function renderApp() {
   const userController = userEvent.setup();
   const result = render(<App />);
-  return { result, userController };
+  return {
+    result,
+    userController,
+    clickButton: makeClickableButton(userController),
+  };
 }
 
 describe("addition", () => {
-  test("3 + 3", async () => {
-    const testTools = renderApp();
-    const threeButton = await screen.findByRole("button", { name: "3" });
-    const sumButton = await screen.findByRole("button", { name: "+" });
-    const totalButton = await screen.findByRole("button", { name: "Total" });
-    await testTools.userController.click(threeButton);
-    await testTools.userController.click(sumButton);
-    await testTools.userController.click(threeButton);
-    await testTools.userController.click(totalButton);
-    expect(screen.getByTestId("display").textContent).toBe("[6]");
+  test.each([
+    { operation: "0+0", total: "0" },
+    { operation: "3+3", total: "6" },
+    { operation: "3+6", total: "9" },
+    { operation: "333+333", total: "666" },
+  ])("$operation = $total", async ({ operation, total }) => {
+    const { clickButton } = renderApp();
+    const clicks = operation.split("").map((buttonName) => {
+      return clickButton(buttonName);
+    });
+    await Promise.all(clicks);
+    await clickButton("Total");
+    expect(screen.getByTestId("display").textContent).toBe(`[${total}]`);
   });
 });
